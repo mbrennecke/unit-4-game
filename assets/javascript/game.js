b2bd560fc156f40afefa0116a212a95a7b4f1d13
Matthew Brennecke <mbrennecke10@gmail.com>
2018-08-04 14:06:57 -0600

    lots of javascript updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,25 +29,43 @@ $(document).ready(function() {
 		attackInc: 12
 	};
 
-function reset() {
-	$("#hideButton").hide();
+function gameReset() {
+	$(".hideButton").hide();
 	champPicked = false;
 	challengerPicked = false;
-	heroCard;
-	challengerCard;
-	elfHealth = 110;
-	mageHealth = 100;
-	orcHealth = 140;
-	goblinHealth = 130;
-	elfAttackPower = 11;
-	mageAttackPower = 10;
-	orcAttackPower = 14;
-	goblinAttackPower = 12;
+	elf.health = 110;
+	mage.health = 100;
+	orc.health = 140;
+	goblin.health = 120;
+	elf.attack = 11;
+	mage.attack = 10;
+	orc.attack = 14;
+	goblin.attack = 12;
+	$("#instructions").html("To begin the game click on your champion");
+}
+
+function cardPlaceReset() {
+	var colPos = [1, 4, 5, 8];
+	for (var i = 1; i <=4; i++){
+		var card = "#card" + i;
+		var slot = "#slot" + colPos[(i-1)];
+		$(card).detach().appendTo(slot);
+		if (slot == 1 || slot == 5) {
+			if ($(card).hasClass("float-right")) {
+			$(card).removeClass("float-right");
+		}
+		}
+	}
+	$(".mage").text("100 HP");
+	$(".goblin").text("120 HP");
+	$(".orc").text("140 HP");
+	$(".elf").text("110 HP");
+
 }
 
 function createHero() {
 	var picked = event.target;
-	picked = $(picked).attr("data-cardElement");
+	picked = $(picked).data("cardelement");
 	heroCard = ("#card" + picked);
 	if ($(heroCard).hasClass("float-right")) {
 		$(heroCard).removeClass("float-right");
@@ -59,7 +77,7 @@ function createHero() {
 
 function createChallenger() {
 	var picked = event.target;
-	picked = $(picked).attr("data-cardElement");
+	picked = $(picked).data("cardelement");
 	challengerCard = ("#card" + picked);
 	if (!$(challengerCard).hasClass("float-right")) {
 		$(challengerCard).addClass("float-right");
@@ -67,10 +85,32 @@ function createChallenger() {
 	$(challengerCard).detach().appendTo("#slot3");
 	challengerPicked = true;
 	$("#instructions").html("Click <span class='attack'>Attack</span> button to attack challenger");
-	$("#hideButton").show();
+	$(".hideButton").show();
 }
 
-function damage(damageCard, attackCard) {
+function dead(whoDead, card) {
+	if (mage.health > 0 && goblin.health > 0 && orc.health > 0 && elf.health > 0) { 
+		return true;
+	}
+	if (whoDead == 1) {
+		challengerDead(card);
+	} 
+	if (whoDead == 2) {
+		heroDead(card);
+	}
+}
+
+function challengerDead(card){
+	
+}
+
+function heroDead(card) {
+	cardPlaceReset();
+	gameReset();
+}
+
+
+function damage(damageCard, attackCard, whoDead) {
 	var attack = 0;
 	if (attackCard == "#card1") {
 		attack = mage.attack;
@@ -84,18 +124,29 @@ function damage(damageCard, attackCard) {
 		
 	if (damageCard == "#card1") {
 		mage.health -= attack;
-		$(".warMage").text(mage.health);
+		if (mage.health < 0) {
+			dead(whoDead, damageCard);
+		}
+		$(".mage").text(mage.health + " HP");
 	} else if (damageCard == "#card2") {
 		goblin.health -= attack;
-		$(".goblin").text(goblin.health);
+		if (goblin.health < 0) {
+			dead(whoDead, damageCard);
+		}
+		$(".goblin").text(goblin.health  + " HP");
 	} else if (damageCard == "#card3") {
 		orc.health -= attack;
-		$(".orc").text(orc.health);
-	} else if (damageCard == "#card4") {
+		if (orc.health < 0) {
+			dead(whoDead, damageCard);
+		}
+		$(".orc").text(orc.health  + " HP");
+	} else if (damageCard == "#card4" ) {
 		elf.health -= attack;
-		$(".elf").text(elf.health);
-	}
-	
+		if (elf.health < 0) {
+			dead(whoDead, damageCard);
+		}
+		$(".elf").text(elf.health  + " HP");
+	}	
 }
 
 function attackModifier (card) {
@@ -120,11 +171,12 @@ $(".card").click(function(event) {
 });
 
 $(".btn").click(function(event) {
-	damage(challengerCard, heroCard);
-	damage(heroCard, challengerCard);
+	damage(challengerCard, heroCard, 1);
+	damage(heroCard, challengerCard, 2);
 	attackModifier(heroCard);
+	
 });
 
-reset();
+gameReset();
 
 });
